b19f5dfd4624b71381272ab406b819e017342775
micsiw <michalsiwanowicz@gmail.com>
2023-03-21 17:15:01 +0100

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <h1>CV Custom Creator</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
