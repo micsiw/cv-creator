0580f0503485002cbc4da8cad253d1788bcb7569
micsiw <michalsiwanowicz@gmail.com>
2023-03-23 15:22:30 +0100

    added some styling to visualise project

---
+++ src/components/CV-Form/forms.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Forms extends Component {
+  render() {
+    return (
+      <div className="forms">
+        <input type="text"></input>
+      </div>
+    );
+  }
+}
+
+export default Forms;
--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Forms from "./CV-Form/forms";
+import Display from "./CV-Display/display";
+import "../styles/Main.css";
 
 class Main extends Component {
   render() {
     return (
       <div className="main">
-        <p>Forms Here</p>
+        <Forms />
+        <Display />
       </div>
     );
   }
